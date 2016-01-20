595f4f288ae47c12b6840c12f66feb97554befaf
Javier Vindas <xavinso@gmail.com>
2016-01-20 17:54:16 -0600

    Refactor to use blueprints

---
+++ project/users/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+
+
+class RegisterForm(Form):
+    name = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Email(), Length(min=6, max=25)]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    confirm = PasswordField(
+        'Repeat Password',
+        validators=[DataRequired(), EqualTo(
+            'password',
+            message='Passwords must match.'
+        )]
+    )
+
+
+class LoginForm(Form):
+    name = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
